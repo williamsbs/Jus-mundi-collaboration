41e7e774a5753d5381ffb73e82aff437543459c2
William Sabates <william@adlin-science.com>
2025-07-04 19:27:10 +0200

    Created index page to display each section

---
+++ pages/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="main-page">
+    <HeaderSection class="header-section" />
+    <QuotesSection class="quotes-section" />
+    <PartnershipSection class="partnership-section" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import HeaderSection from "~/components/sections/header-section.vue";
+import PartnershipSection from "~/components/sections/partnership-section.vue";
+import QuotesSection from "~/components/sections/quotes-section.vue";
+</script>
+
+<style scoped>
+.main-page {
+  min-height: 100vh;
+}
+
+.header-section {
+  background: linear-gradient(180deg, #ffffff 87.38%, #f6f6f6 99.55%);
+  position: relative;
+  height: 800px;
+  overflow: hidden;
+  padding: 3rem 4rem;
+}
+
+.quotes-section {
+  padding: 4rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.partnership-section {
+  padding: 1rem 4rem 4rem;
+  display: flex;
+  justify-content: center;
+}
+</style>
