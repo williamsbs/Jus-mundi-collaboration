0c2c2c7aa7f44923299ee7fef2b2aac6b4b00dcd
William Sabates <william@adlin-science.com>
2025-07-04 20:31:57 +0200

    use @nuxt/font to load fonts instead of doing it manually

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,27 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxt/eslint", "@nuxt/fonts"],
   css: ["~/assets/css/main.css"],
+  fonts: {
+    families: [
+      {
+        name: "IBM Plex Sans",
+        provider: "google",
+        weights: [100, 200, 300, 400, 500, 600, 700],
+        styles: ["normal", "italic"],
+      },
+      {
+        name: "Lato",
+        provider: "google",
+        weights: [400, 700],
+      },
+      {
+        name: "Larken",
+        weights: [100, 200, 300, 400, 500, 600, 700, 800],
+        styles: ["normal", "italic"],
+        provider: "local",
+      },
+    ],
+  },
   app: {
     head: {
       title: "Jus mundi collaboration",
